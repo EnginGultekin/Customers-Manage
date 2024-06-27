c63afca819b86b2c5467bca0a209f70ba1d78bd4
Engin Gültekin <engingultekin1071@gmail.com>
2024-06-27 11:18:25 +0300

    Singleton design pattern created for database connectivity

--- CustomerSystem/src/App.java
+++ CustomerSystem/src/App.java
@@ -1,20 +1,11 @@
+import core.Database;
 import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class App {
     public static void main(String[] args) {
 
-        try {
-            Connection connection = DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/customer_manage",
-                    "root",
-                    "Savas1453.."
-            );
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        Connection connection1 = Database.getInstance();
+        Connection connection2 = Database.getInstance();
+
     }
 }
